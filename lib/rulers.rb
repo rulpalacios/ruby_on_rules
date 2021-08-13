e8a286a0ad7361c910660c1a0216b2b7e6cc0f1a
Raul Palacios <palacios.raul02@gmail.com>
2021-08-12 20:32:59 -0500

    Add array class to rulers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,14 @@
+require "rulers/array"
 require "rulers/version"
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Application
+    def call(env)
+      `echo debug > debug.txt`;
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from Ruby on Rulers!"]]
+    end
+  end
 end
