d7ea9a1e3ed30b232a71e7b553e5400aa3dc4658
Raul Palacios <palacios.raul02@gmail.com>
2021-08-16 15:26:29 -0500

    Add dependencies and utilities

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,15 @@
 require "rulers/version"
 require "rulers/routing"
+require "rulers/util"
+require "rulers/dependencies"
 
 module Rulers
-  class Error < StandardError; end
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
@@ -22,5 +27,5 @@ module Rulers
       @env
     end
   end
-  
+
 end
